8475f9fba640f9a4308b2680687085ed4f690076
Kevin Morio <kevin@morio.me>
2021-02-01 10:27:56 +0100

    zsh: Add emacs to path on macOS

--- zsh/macos.zsh
+++ zsh/macos.zsh
@@ -3,6 +3,9 @@
 # Use colored output for cli commands.
 export CLICOLOR=1
 
+# Make Emacs available on the command-line.
+export PATH="$PATH:$HOME/src/emacs/nextstep/Emacs.app/Contents/MacOS"
+
 # iTerm2 shell integration
 if [[ -e "${HOME}/.iterm2_shell_integration.zsh" ]]; then
     source "${HOME}/.iterm2_shell_integration.zsh"
